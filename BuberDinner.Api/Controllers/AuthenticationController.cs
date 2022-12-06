8daabf06d3e00a054853a1873251a697a84302ec
Jeffreyyvdb <jeffrey.2001.2312@gmail.com>
2022-12-06 23:40:01 +0100

    Use of OneOf is you return one error

--- BuberDinner.Api/Controllers/AuthenticationController.cs
+++ BuberDinner.Api/Controllers/AuthenticationController.cs
@@ -1,7 +1,8 @@
-using BuberDinner.Api.Filters;
+using BuberDinner.Application.Common.Errors;
 using BuberDinner.Application.Services.Authentication;
 using BuberDinner.Contracts.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using OneOf;
 
 namespace BuberDinner.Api.Controllers;
 
@@ -18,20 +19,25 @@ private readonly IAuthenticationService _authenticationService;
 
     [HttpPost("register")]
     public IActionResult Register(RegisterRequest request){
-        var authResult = _authenticationService.Register(
+        OneOf<AuthenticationResult, IError> registerResult = _authenticationService.Register(
             request.FirstName,
             request.LastName,
             request.Email,
             request.Password);
 
-        var response = new AuthenticationResponse(
-            authResult.user.Id,
-            authResult.user.FirstName,
-            authResult.user.LastName,
-            authResult.user.Email,
-            authResult.Token);
+        return registerResult.Match(
+            authResult => Ok(MapAuthResult(authResult)),
+            error => Problem(statusCode: (int)error.StatusCode, title: error.ErrorMessage));
+    }
 
-        return Ok(response);
+    private static AuthenticationResponse MapAuthResult(AuthenticationResult authResult)
+    {
+        return new AuthenticationResponse(
+                        authResult.user.Id,
+                        authResult.user.FirstName,
+                        authResult.user.LastName,
+                        authResult.user.Email,
+                        authResult.Token);
     }
 
     [HttpPost("login")]
--- BuberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,6 +1,8 @@
+using BuberDinner.Application.Common.Errors;
 using BuberDinner.Application.Common.Interfaces.Authentication;
 using BuberDinner.Application.Persistence;
 using BuberDinner.Domain.Entities;
+using OneOf;
 
 namespace BuberDinner.Application.Services.Authentication;
 
@@ -15,12 +17,12 @@ public class AuthenticationService : IAuthenticationService
         _userRepository = userRepository;
     }
 
-    public AuthenticationResult Register(string firstName, string lastName, string email, string password)
+    public OneOf<AuthenticationResult, IError> Register(string firstName, string lastName, string email, string password)
     {
         // 1. Validate the user doesn't exist
         if(_userRepository.GetUserByEmail(email) is not null)
         {
-            throw new Exception("User with given email already exists");
+            return new DuplicateEmailError();
         }
 
         // 2. Create user (generate unique ID) & Persist to DB
