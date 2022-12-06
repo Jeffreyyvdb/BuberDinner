c6fccd5fc63b18a77e2f12f3bc0788240c23d17f
Jeffreyyvdb <jeffrey.2001.2312@gmail.com>
2022-12-07 00:45:56 +0100

    Implement ErrorOr

--- BuberDinner.Api/Controllers/AuthenticationController.cs
+++ BuberDinner.Api/Controllers/AuthenticationController.cs
@@ -1,14 +1,16 @@
 using BuberDinner.Application.Services.Authentication;
 using BuberDinner.Contracts.Authentication;
+using ErrorOr;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BuberDinner.Api.Controllers;
 
 [ApiController]
 [Route("auth")]
-public class AuthenticationController : ControllerBase{
-    
-private readonly IAuthenticationService _authenticationService;
+public class AuthenticationController : ControllerBase
+{
+
+    private readonly IAuthenticationService _authenticationService;
 
     public AuthenticationController(IAuthenticationService authenticationService)
     {
@@ -16,12 +18,27 @@ private readonly IAuthenticationService _authenticationService;
     }
 
     [HttpPost("register")]
-    public IActionResult Register(RegisterRequest request){
-        var authResult = _authenticationService.Register(
+    public IActionResult Register(RegisterRequest request)
+    {
+        ErrorOr<AuthenticationResult> authResult = _authenticationService.Register(
             request.FirstName,
             request.LastName,
             request.Email,
             request.Password);
+
+        return authResult.MatchFirst(
+            authResult => Ok(MapAuthResult(authResult)),
+            firstError => Problem(statusCode: StatusCodes.Status409Conflict, title: firstError.Description)
+            );
+    }
+
+
+    [HttpPost("login")]
+    public IActionResult Login(LoginRequest request)
+    {
+        var authResult = _authenticationService.Login(
+        request.Email,
+        request.Password);
 
         var response = new AuthenticationResponse(
             authResult.user.Id,
@@ -32,20 +49,13 @@ private readonly IAuthenticationService _authenticationService;
 
         return Ok(response);
     }
-
-    [HttpPost("login")]
-    public IActionResult Login(LoginRequest request){
-            var authResult = _authenticationService.Login(
-            request.Email,
-            request.Password);
-
-        var response = new AuthenticationResponse(
-            authResult.user.Id,
-            authResult.user.FirstName,
-            authResult.user.LastName,
-            authResult.user.Email,
-            authResult.Token);
-
-        return Ok(response);
+    private static AuthenticationResponse MapAuthResult(AuthenticationResult authResult)
+    {
+        return new AuthenticationResponse(
+                    authResult.user.Id,
+                    authResult.user.FirstName,
+                    authResult.user.LastName,
+                    authResult.user.Email,
+                    authResult.Token);
     }
 }
