454502e1bfe5cb4ef419b5c5d2a3ccf8eded379a
Jeffreyyvdb <jeffrey.2001.2312@gmail.com>
2022-12-11 15:24:12 +0100

    Part 7 Add object mapping with mapster

--- BuberDinner.Api/Controllers/AuthenticationController.cs
+++ BuberDinner.Api/Controllers/AuthenticationController.cs
@@ -3,9 +3,10 @@ using BuberDinner.Application.Authentication.Common;
 using BuberDinner.Application.Authentication.Queries.Login;
 using BuberDinner.Contracts.Authentication;
 using ErrorOr;
+using MapsterMapper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using static BuberDinner.Domain.Common.Errors;
+using static BuberDinner.Domain.Common.Errors.Errors;
 
 namespace BuberDinner.Api.Controllers;
 
@@ -13,20 +14,24 @@ namespace BuberDinner.Api.Controllers;
 public class AuthenticationController : ApiController
 {
     private readonly ISender _mediator;
+    private readonly IMapper _mapper;
 
-    public AuthenticationController(ISender mediator)
+    public AuthenticationController(
+        ISender mediator,
+        IMapper mapper)
     {
         _mediator = mediator;
+        _mapper = mapper;
     }
 
     [HttpPost("register")]
     public async Task<IActionResult> Register(RegisterRequest request)
     {
-        var command = new RegisterCommand(request.FirstName, request.LastName, request.Email, request.Password);
+        var command = _mapper.Map<RegisterCommand>(request);
         ErrorOr<AuthenticationResult> authResult = await _mediator.Send(command);
 
         return authResult.Match(
-            authResult => Ok(MapAuthResult(authResult)),
+            authResult => Ok(_mapper.Map<AuthenticationResponse>(authResult)),
             errors => Problem(errors)
             );
     }
@@ -35,7 +40,7 @@ public class AuthenticationController : ApiController
     [HttpPost("login")]
     public async Task<IActionResult> Login(LoginRequest request)
     {
-        var query = new LoginQuery(request.Email, request.Password);
+        var query = _mapper.Map<LoginQuery>(request);
         var authResult = await _mediator.Send(query);
 
         if (authResult.IsError && authResult.FirstError == Authentication.InvalidCredentials)
@@ -46,18 +51,8 @@ public class AuthenticationController : ApiController
         }
 
         return authResult.Match(
-            authResult => Ok(MapAuthResult(authResult)),
+            authResult => Ok(_mapper.Map<AuthenticationResponse>(authResult)),
             errors => Problem(errors)
             );
-    }
-
-    private static AuthenticationResponse MapAuthResult(AuthenticationResult authResult)
-    {
-        return new AuthenticationResponse(
-                    authResult.User.Id,
-                    authResult.User.FirstName,
-                    authResult.User.LastName,
-                    authResult.User.Email,
-                    authResult.Token);
     }
 }
--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,17 +1,15 @@
-using BuberDinner.Api.Errors;
+using BuberDinner.Api;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services
+        .AddPresentatioin()
         .AddApplication()
-        .AddInfrastructure(builder.Configuration); ;
+        .AddInfrastructure(builder.Configuration);
         
-    builder.Services.AddControllers();
 
-    builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
 }
 
 var app = builder.Build();
