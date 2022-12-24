801f2c7079c016b17ee6d9018a009b0f9a288fa3
Jeffreyyvdb <jeffrey.2001.2312@gmail.com>
2022-12-24 14:00:30 +0100

    Implementing a RegisterCommand validation pipline behavior

---
+++ BuberDinner.Application/Common/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,21 @@
+﻿using BuberDinner.Application.Authentication.Commands.Register;
+using BuberDinner.Application.Authentication.Common;
+using ErrorOr;
+using MediatR;
+
+namespace BuberDinner.Application.Common.Behaviors;
+public class ValidationRegisterCommmandBehavior :
+    IPipelineBehavior<RegisterCommand, ErrorOr<AuthenticationResult>>
+{
+    public async Task<ErrorOr<AuthenticationResult>> Handle(
+        RegisterCommand request,
+        RequestHandlerDelegate<ErrorOr<AuthenticationResult>> next,
+        CancellationToken cancellationToken)
+    {
+        // beofre the handler
+        var result = await next();
+        // after the handler
+
+        return result;
+    }
+}
